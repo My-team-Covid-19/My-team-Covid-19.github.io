909f4d38242c927959a27c173170a295e8c73b5a
Shukevich Kseniya <Kseniya.Shukevich@gmail.com>
2020-12-18 02:39:14 +0300

    feat: add map

--- covid/src/page/render.js
+++ covid/src/page/render.js
@@ -1,10 +1,13 @@
+import getDefaultMap from '../map/map';
+
 export default function getPage(data) {
-  console.log(data.globalData.NewConfirmed);
-  for (let i = 0; i < data.countriesData.length; i += 1) {
-    console.log(data.countriesData[i].TotalDeaths);
-  }
-  for (let i = 0; i < data.flagsAndPopulations.length; i += 1) {
-    console.log(data.flagsAndPopulations[i].name);
-    console.log(data.flagsAndPopulations[i].population);
-  }
+  getDefaultMap(data);
+  // console.log(data.globalData.NewConfirmed);
+  // for (let i = 0; i < data.countriesData.length; i += 1) {
+  //   console.log(data.countriesData[i].TotalDeaths);
+  // }
+  // for (let i = 0; i < data.flagsAndPopulations.length; i += 1) {
+  //   console.log(data.flagsAndPopulations[i].name);
+  //   console.log(data.flagsAndPopulations[i].population);
+  // }
 }

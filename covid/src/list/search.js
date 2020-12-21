2a8b20b5a15f3c4db043ac7ff67144bc70e21bdf
cptShell <repozheka@gmail.com>
2020-12-22 00:42:06 +0300

    feat: add searhing for country list (alpha)

--- covid/src/list/search.js
+++ covid/src/list/search.js
@@ -1,5 +1,15 @@
 const input = document.querySelector('.search-input');
 
 input.addEventListener('input', () => {
-  console.log(input.value);
+  const items = [...document.querySelectorAll('.country-list-item')];
+  const inputVal = input.value;
+
+  items.forEach((elem) => {
+    const country = elem.querySelector('.name').textContent;
+    if (!country.includes(inputVal)) {
+      elem.classList.add('hidden');
+    } else {
+      elem.classList.remove('hidden');
+    }
+  });
 });

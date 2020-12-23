4779a72c9283422118104ff48c143ddbae510d87
Shukevich Kseniya <Kseniya.Shukevich@gmail.com>
2020-12-23 03:33:58 +0300

    refactor: try to change folder name

--- covid/src/page/setUpateTime.js
+++
@@ -1,5 +0,0 @@
-export default function lastUpdate(data) {
-  const dateElem = document.querySelector('.date');
-  const date = new Date(data.globalData.updated);
-  dateElem.textContent = `${date.toDateString()} ${date.getHours()}:${date.getMinutes()}`;
-}
